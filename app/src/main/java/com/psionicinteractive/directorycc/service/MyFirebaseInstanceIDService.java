4d941d953a2ffff2272e077a4688d1c8dbf5de48
iushaheed <iushaheed@gmail.com>
2016-10-19 13:06:41 +0600

    IUS|push updated with parbesh

--- app/src/main/java/com/psionicinteractive/directorycc/service/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/psionicinteractive/directorycc/service/MyFirebaseInstanceIDService.java
@@ -14,34 +14,34 @@ import com.psionicinteractive.directorycc.app.Config;
 
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+    private static final String REG_TOKEN = MyFirebaseInstanceIDService.class.getSimpleName();
 
     @Override
     public void onTokenRefresh() {
-        super.onTokenRefresh();
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN,recent_token);
 
         // Saving reg id to shared preferences
-        storeRegIdInPref(refreshedToken);
-
-        // sending reg id to your server
-        sendRegistrationToServer(refreshedToken);
-
-        // Notify UI that registration has completed, so the progress indicator can be hidden.
-        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
-        registrationComplete.putExtra("token", refreshedToken);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+//        storeRegIdInPref(refreshedToken);
+//
+//        // sending reg id to your server
+//        sendRegistrationToServer(refreshedToken);
+//
+//        // Notify UI that registration has completed, so the progress indicator can be hidden.
+//        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+//        registrationComplete.putExtra("token", refreshedToken);
+//        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
     }
 
-    private void sendRegistrationToServer(final String token) {
-        // sending gcm token to server
-        Log.e(TAG, "sendRegistrationToServer: " + token);
-    }
-
-    private void storeRegIdInPref(String token) {
-        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("regId", token);
-        editor.commit();
-    }
+//    private void sendRegistrationToServer(final String token) {
+//        // sending gcm token to server
+//        Log.e(TAG, "sendRegistrationToServer: " + token);
+//    }
+//
+//    private void storeRegIdInPref(String token) {
+//        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+//        SharedPreferences.Editor editor = pref.edit();
+//        editor.putString("regId", token);
+//        editor.commit();
+//    }
 }
